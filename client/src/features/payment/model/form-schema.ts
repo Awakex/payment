ffa1441fca3d9170a7d54b03e555ca1fd37b3c17
Igor Zakharov <awkworkx@gmail.com>
2024-01-03 17:50:39 +0300

    feat: Added expire validation
    Signed-off-by: Igor Zakharov <awkworkx@gmail.com>

--- client/src/features/payment/model/form-schema.ts
+++ client/src/features/payment/model/form-schema.ts
@@ -2,12 +2,22 @@ import { z } from "zod";
 
 export const formSchema = z.object({
   pan: z.string().regex(/^\d+$/).min(13).max(19),
-  cvc: z.string().min(3).max(3),
-  expire: z.string(),
+  cvc: z.string().regex(/^\d+$/).min(3).max(3),
+  expire: z.string().refine((v) => {
+    const [month, year] = v.split("/");
+
+    if (month && year) {
+      return (
+        Number(month) >= 1 &&
+        Number(month) <= 12 &&
+        Number(Number(year) >= 21 && Number(year) <= 26)
+      );
+    }
+  }),
   cardholder: z
     .string()
     .regex(/^([^0-9]*)$/)
-    .refine((v) => v.split(" ").length > 1),
+    .refine((v) => v.trim().split(" ").length > 1),
 });
 
 export type FormSchemaType = z.infer<typeof formSchema>;
